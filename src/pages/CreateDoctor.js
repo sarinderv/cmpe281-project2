37f920e14fa706b70003734217f260a710c14583
serdardemirci55 <89255090+serdardemirci55@users.noreply.github.com>
2021-11-08 00:15:12 +0300

    Create Doctor and List Doctor details added

--- src/pages/CreateDoctor.js
+++ src/pages/CreateDoctor.js
@@ -1,10 +1,25 @@
 import { Auth } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
+import { useFormFields } from "../lib/hooksLib";
+import { API } from 'aws-amplify';
+import "./CreateDoctor.css";
+import { createDoctor } from '../graphql/mutations';
 
-function CreateDoctor() {
+export default function CreateDoctor() {
 
     const [userData, setUserData] = useState({ payload: { username: '' } });
-
+    const [errorMessages, setErrorMessages] = useState([]);
+    const [fields, handleFieldChange] = useFormFields({
+      fistName: "",
+      lastName: "",
+      phone: "",
+      address: "",
+    });
+    const history = useHistory();
+  
     useEffect(() => {
       fetchUserData();
       }, []);
@@ -18,11 +33,71 @@ function CreateDoctor() {
         .catch((e) => console.log("Not signed in", e));
     }
 
-    return (
-        <div className='createdoctor'>
-            <h1>Create Doctor</h1>
-        </div>
-    );
+    function validateForm() {
+      try {
+        return (
+          fields.firstName.length > 0 &&
+          fields.lastName.length > 0 &&
+          fields.phone.length > 0 &&
+          fields.address.length > 0
+        );
+      } catch (e) {
+        return false;
+      }
+    }
+  
+    async function handleSubmit(event) {
+      event.preventDefault();
+      try {
+        await API.graphql({ query: createDoctor, variables: { input: {firstName: fields.firstName, lastName: fields.lastName, id: userData.payload.username, phone: fields.phone, address: fields.address} } });
+      } catch (e) {
+        console.error('error creating doctor', e);
+        setErrorMessages(e.errors);
+      }
+      history.push("/doctor");
+    }
+  
+    function renderForm() {
+      return (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="firstName" size="lg">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={fields.firstName}
+              onChange={handleFieldChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="lastName" size="lg">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={fields.lastName}
+              onChange={handleFieldChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="phone" size="lg">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="text"
+              value={fields.phone}
+              onChange={handleFieldChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="address" size="lg">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              value={fields.address}
+              onChange={handleFieldChange}
+            />
+          </Form.Group>
+          <Button block size="lg" type="submit" disabled={!validateForm()}>
+            Create
+          </Button>
+        </Form>
+      );
+    }
+  
+    return <div className="createdoctor"> <h1>Create Doctor</h1> {renderForm()} </div>;
 }
-
-export default CreateDoctor;
--- src/pages/Doctor.js
+++ src/pages/Doctor.js
@@ -3,6 +3,8 @@ import { API } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
 import { getDoctor } from '../graphql/queries';
 import { useHistory } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 function Doctor() {
 
@@ -10,6 +12,7 @@ function Doctor() {
     const [doctor, setDoctor] = useState({ });
     const [errorMessages, setErrorMessages] = useState([]);
     const history = useHistory();
+    const [addModalShow, setAddModalShow] = React.useState(false);
 
     useEffect(() => {
       fetchUserData();
@@ -35,14 +38,43 @@ function Doctor() {
         } catch (e) {
             console.error('error fetching doctor', e);
             setErrorMessages(e.errors);
-          }
+        }
       }
+    
+    function updateDoctor(id) {
+    }
 
     return (
         <div className='doctor'>
-            <h1>Doctor</h1>
-            {doctor.firstName}
-            {doctor.lastName}
+          <h1>Doctor</h1>
+              <div>
+                    <Container>
+                      <Row className="align-items-center">
+                        <Col style={{ fontSize: "1rem" }}>
+                          <b>Name: </b> {doctor.firstName}
+                        </Col>
+                        <Col style={{ fontSize: "1rem" }}>
+                          <b>Last Name:</b> {doctor.lastName}
+                        </Col>
+                        <Col style={{ fontSize: "1rem" }}>
+                          <b>Phone:</b> {doctor.phone}
+                        </Col>
+                        <Col style={{ fontSize: "1rem" }}>
+                          <b>Address:</b> {doctor.address}
+                        </Col>
+                        <Col>
+                            <Button
+                              variant="success"
+                              block
+                              size="sm"
+                              onClick={() => updateDoctor(doctor.id)}
+                            >
+                              Update
+                            </Button>
+                        </Col>
+                      </Row>
+                    </Container>
+              </div>
         </div>
     );
 }
