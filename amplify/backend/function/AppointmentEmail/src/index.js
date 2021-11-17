cc5ef7ff181dd8cf5a63b2caf7318e8ecf65af91
Sarinder Virk <sarinder.virk@oracle.com>
2021-11-17 09:47:51 -0800

    appointment email lambda

--- amplify/backend/function/AppointmentEmail/src/index.js
+++ amplify/backend/function/AppointmentEmail/src/index.js
@@ -1,12 +1,45 @@
-
+const AWS = require('aws-sdk')
+const ses = new AWS.SES({ region: 'us-west-2' });
 
 exports.handler = event => {
   //eslint-disable-line
   console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach(record => {
+  var record = event.Records[0];
+    const op = record.eventName;
     console.log(record.eventID);
-    console.log(record.eventName);
+    console.log(op);
     console.log('DynamoDB Record: %j', record.dynamodb);
-  });
-  return Promise.resolve('Successfully processed DynamoDB record');
+
+    if (op === "INSERT") {
+      let docName = record.dynamodb.NewImage.doctorId.S;
+      let time = record.dynamodb.NewImage.appointmentTime.S;
+      let subject = "New appointment with Dr. " + docName;
+      let body = "At " + time;
+      console.log(subject);
+      console.log(body);
+      return sendMail(subject, body);
+    }
+  return Promise.resolve('failed to send AppointmentEmail for DynamoDB record');
 };
+
+function sendMail(subject, data) {
+  const params = {
+    Destination: {
+      ToAddresses: ["sarinder.virk@sjsu.edu"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: data },
+      },
+      Subject: { Data: subject },
+    },
+    Source: "savirk@cmpe281.com",
+  };
+
+  try {
+    return ses.sendEmail(params).promise();
+  } catch (e) {
+    console.log("FAILURE IN SENDING MAIL!!", e);
+  }
+  return;
+}
