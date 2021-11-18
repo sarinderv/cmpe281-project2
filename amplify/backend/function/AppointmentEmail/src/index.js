cc4abad084f4438ad40b8418d40203273f7c2abc
Sarinder Virk <sarinder.virk@oracle.com>
2021-11-17 19:37:18 -0800

    email and subject from dynamodb

--- amplify/backend/function/AppointmentEmail/src/index.js
+++ amplify/backend/function/AppointmentEmail/src/index.js
@@ -1,31 +1,74 @@
 const AWS = require('aws-sdk')
-const ses = new AWS.SES({ region: 'us-west-2' });
+const ses = new AWS.SES({ region: process.env.REGION });
+const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
-exports.handler = event => {
+exports.handler = async function(event) {
   //eslint-disable-line
-  console.log(JSON.stringify(event, null, 2));
-  var record = event.Records[0];
-    const op = record.eventName;
-    console.log(record.eventID);
-    console.log(op);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+  const record = event.Records[0];
+  const op = record.eventName;
+  console.log('process.env: ', JSON.stringify(process.env, null, 2));
+  console.log('Incoming event: ', JSON.stringify(event, null, 2));
+  console.log('DynamoDB Record: ', JSON.stringify(record.dynamodb, null, 2));
 
-    if (op === "INSERT") {
-      let docName = record.dynamodb.NewImage.doctorId.S;
-      let time = record.dynamodb.NewImage.appointmentTime.S;
-      let subject = "New appointment with Dr. " + docName;
-      let body = "At " + time;
-      console.log(subject);
-      console.log(body);
-      return sendMail(subject, body);
-    }
+  if (op === "INSERT") {
+    let docId = record.dynamodb.NewImage.doctorId.S;
+    let patId = record.dynamodb.NewImage.patientId.S;
+    let date = record.dynamodb.NewImage.appointmentDate.S;
+    let time = record.dynamodb.NewImage.appointmentTime.S;
+
+    // no luck - running into IAM permissions issue!!!
+    // getUserdataFromCognito('larry').then((userdata) => {
+    //   userdata ? console.log('email: ', userdata.Attributes.find((k, v) => k.Name === 'email')) : console.log('NOT FOUND');
+    // });
+
+    let patient = await getUserdataFromDynamoDB('Patient-3fpg5psmbbcuzbhfrnty72ryh4', patId);
+    let doctor = await getUserdataFromDynamoDB('Doctor-3fpg5psmbbcuzbhfrnty72ryh4', docId);
+    let subject = `New appointment with Dr. ${doctor.firstName.S} ${doctor.lastName.S}`;
+    let body = `At ${date} ${time}`;
+
+    console.log(subject);
+    console.log(body);
+    return sendMail(subject, body, patient.email.S);
+  }
   return Promise.resolve('failed to send AppointmentEmail for DynamoDB record');
 };
 
-function sendMail(subject, data) {
+async function getUserdataFromDynamoDB(tablename, id) {
+  var params = {
+    TableName: tablename + '-' + process.env.ENV,
+    Key: {
+      'id': { S: id }
+    }
+  }
+
+  // Call DynamoDB to read the item from the table
+  const data = await ddb.getItem(params).promise()
+  return data ? data.Item : null;
+}
+
+async function getUserdataFromCognito(username) {
+  const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+  const params = {
+    UserPoolId: process.env.userPoolId,
+    Filter: `username = "${username}"`,
+  }
+  try {
+    const data = await cognitoidentityserviceprovider.listUsers(params).promise()
+    const existingUser = data.Users.filter(user => user.UserStatus !== 'EXTERNAL_PROVIDER')[0]
+    if (existingUser == null) {
+      console.log('Error', 'User not found')
+    }
+    return existingUser
+  } catch (error) {
+    console.log('Error: getUserByAttribute', error)
+    return null;
+  }
+}
+
+function sendMail(subject, data, email) {
   const params = {
     Destination: {
-      ToAddresses: ["sarinder.virk@sjsu.edu"],
+      ToAddresses: [email],
     },
     Message: {
       Body: {
@@ -33,7 +76,7 @@ function sendMail(subject, data) {
       },
       Subject: { Data: subject },
     },
-    Source: "savirk@cmpe281.com",
+    Source: "appointment@cmpe281.com",
   };
 
   try {
