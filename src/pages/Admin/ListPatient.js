c269eb97d76546cf4241536afc405dddd6eb56d1
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2021-11-22 13:07:58 -0800

    added Update Patient functionality

--- src/pages/Admin/ListPatient.js
+++ src/pages/Admin/ListPatient.js
@@ -15,10 +15,9 @@ import {
 } from "react-bootstrap";
 import { createDoctor, deleteDoctor, deletePatient } from '../../graphql/mutations';
 import { useFormFields } from '../../lib/hooksLib';
-import UpdateDoctorModal from '../UpdateDoctorModal';
+import UpdatePatientModal from '../UpdatePatientModal';
 
-export default function ListPatient()
-{
+export default function ListPatient() {
   const [show, setShow] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const handleClose = () => setShow(false);
@@ -48,30 +47,27 @@ export default function ListPatient()
     sex: "",
     ssn: "",
     insuranceNumber: ""
-    });
+  });
   const [errorMessages, setErrorMessages] = useState([]);
 
   useEffect(() => {
     list();
 
-  },[]);
-  async function list()
-  {
-    try{
+  }, []);
+  async function list() {
+    try {
       const apiData = await API.graphql({ query: listPatients });
 
       console.log(apiData);
 
-    if (apiData.data.listPatients.items == null)
-    {
-      history.push('/createpatient')
-    }
+      if (apiData.data.listPatients.items == null) {
+        history.push('/createpatient')
+      }
 
-    setListPat(apiData.data.listPatients.items);
+      setListPat(apiData.data.listPatients.items);
 
-    } catch (e)
-    {
-      console.log("Errors fetching Patients...",e);
+    } catch (e) {
+      console.log("Errors fetching Patients...", e);
     }
 
   }
@@ -91,13 +87,13 @@ export default function ListPatient()
 
   async function getPatientInfo(id) {
     try {
-      const apiData = await API.graphql({ query: getPatient, variables: { id: id } } );
+      const apiData = await API.graphql({ query: getPatient, variables: { id: id } });
       //console.log(apiData);
       if (apiData.data.getPatient == null) {
         history.push("/createpatient")
       }
       setSinglePatient({
-        id:apiData.data.getPatient.id,
+        id: apiData.data.getPatient.id,
         fistName: apiData.data.getPatient.firstName,
         lastName: apiData.data.getPatient.lastName,
         phone: apiData.data.getPatient.phone,
@@ -105,23 +101,20 @@ export default function ListPatient()
         birthDate: apiData.data.getPatient.birthDate,
         sex: apiData.data.getPatient.sex,
         ssn: apiData.data.getPatient.ssn,
-        insuranceNumber: apiData.data.getPatient.insuranceNumber        
+        insuranceNumber: apiData.data.getPatient.insuranceNumber
       });
 
       setUpdateModalShow(true)
     } catch (e) {
-        console.error('error fetching Patient...', e);
-        setErrorMessages(e.errors);
+      console.error('error fetching Patient...', e);
+      setErrorMessages(e.errors);
     }
   }
 
-  async function deletePatientInfo(id)
-  {
-    try
-    {
-      await API.graphql({ query: deletePatient, variables: { input: { id : id}}});
-    } catch(e)
-    {
+  async function deletePatientInfo(id) {
+    try {
+      await API.graphql({ query: deletePatient, variables: { input: { id: id } } });
+    } catch (e) {
       console.error('error deleting patient', e);
       setErrorMessages(e.errors);
     }
@@ -130,58 +123,59 @@ export default function ListPatient()
 
   }
 
-  return(
+  return (
     <Container>
       <Row>
         <Col className="m-3">
-          <Button variant="primary" onClick={ () => {
+          <Button variant="primary" onClick={() => {
             history.push("/createpatient")
-          } }>Add New Patient</Button>
-          
+          }}>Add New Patient</Button>
+
         </Col>
         <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>Name</th>
-            <th>Phone Number</th>
-            <th>Address</th>
-            <th>Birth Date</th>
-            <th>Sex</th>
-            <th>SSN</th>
-            <th>Insurance Number</th>
-            <th>Created At</th>
-            <th>Updated At</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-        { listPat.map(element => (
-          <tr key={element.id}>
-            <td>{element.id}</td>
-            <td>{element.firstName} {element.lastName}</td>
-            <td>{element.phone}</td>
-            <td>{element.address}</td>
-            <td>{element.birthDate}</td>
-            <td>{element.sex}</td>
-            <td>{element.ssn}</td>
-            <td>{element.insuranceNumber}</td>
-            <td>{element.createdAt}</td>
-            <td>{element.updatedAt}</td>
-            <td>
-              <Button variant="primary" onClick={() => getPatientInfo(element.id)}>Edit</Button>{' '}
-              <Button variant="danger" onClick={() => deletePatientInfo(element.id)}>Delete</Button>
-            </td>
-          </tr>
-          )) }
-        </tbody>
-      </Table>
-      <UpdateDoctorModal
-                  show={updateModalShow}
-                  doctor={singlePatient}
-                  onUpdated={() => getPatientInfo(singlePatient.id)}
-                  onHide={() => setUpdateModalShow(false)}
-                />
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+              <th>Birth Date</th>
+              <th>Sex</th>
+              <th>SSN</th>
+              <th>Insurance Number</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listPat.map(element => (
+              <tr key={element.id}>
+                <td>{element.id}</td>
+                <td>{element.firstName} {element.lastName}</td>
+                <td>{element.phone}</td>
+                <td>{element.address}</td>
+                <td>{element.birthDate}</td>
+                <td>{element.sex}</td>
+                <td>{element.ssn}</td>
+                <td>{element.insuranceNumber}</td>
+                <td>{element.createdAt}</td>
+                <td>{element.updatedAt}</td>
+                <td>
+                  <Button variant="primary" onClick={() => getPatientInfo(element.id)}>Edit</Button>{' '}
+                  <Button variant="danger" onClick={() => deletePatientInfo(element.id)}>Delete</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        <UpdatePatientModal
+          show={updateModalShow}
+          patient={singlePatient}
+          onUpdated={() => getPatientInfo(singlePatient.id)}
+          onHide={() => setUpdateModalShow(false)}
+        />
       </Row>
     </Container>
   )
